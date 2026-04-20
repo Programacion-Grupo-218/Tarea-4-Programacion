65fa11e5c4e6ebb86b38c36e57008a403de525ba
unknown <brinssoncardenas@gmail.com>
2026-04-20 10:24:21 -0500

    teste primer commit

---
+++ Sistema Integral de Gestión de Clientes, Servicios y Reservas.py
@@ -0,0 +1,3 @@
+# CODIGO PRINCIPAL
+# Cualquier cambio de cada integrante se pondra primero en las ramas de github, si esta correcto se hara un merge a la rama principal,
+# y luego se subira a github, para que asi cada integrante pueda tener el codigo actualizado
